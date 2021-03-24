61e2b41fb498e279c8d5cc212bac3b17e6735d7c
Gene Chuang <genechuang@gmail.com>
2021-03-24 10:57:39 -0700

    Streamlined block - no unfurl, compact with thin image, update block instead of delete/add, search results @author

---
+++ goSAL/main.go
@@ -0,0 +1,27 @@
+// Package helloworld provides a set of Cloud Functions samples.
+// Deploy: gcloud functions deploy HelloHTTP --project=sal9000-307923 --runtime go113 --trigger-http --allow-unauthenticated  --region=us-west2
+
+package helloworld
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
+
+// HelloHTTP is an HTTP Cloud Function with a request parameter.
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Hello, World!")
+		return
+	}
+	if d.Name == "" {
+		fmt.Fprint(w, "Hello, World!")
+		return
+	}
+	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+}
